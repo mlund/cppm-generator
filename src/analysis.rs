33494efd543207f41da974c929c186d9b805ed58
Mikael Lund <Mlund@mac.com>
2022-04-20 16:09:59 +0200

    Add progressbar

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,3 +1,23 @@
+// Copyright (c) 2022 Mikael Lund
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use crate::particle::Particle;
 use nalgebra::Vector3;
 
@@ -48,10 +68,12 @@ impl Moments {
 
     pub fn print(&self) {
         let cog = self.geometric_center.transpose() / self.number_of_samples as f64;
-        println!("cog = |⟨∑𝐫ᵢ/N⟩|     = {:.1} Å", cog.norm());
+        println!("geometric center displacement = |⟨∑𝐫ᵢ/N⟩| = {:.1} Å", cog.norm());
+
         let coc = self.charge_center.transpose() / self.number_of_samples as f64;
-        println!("coc = |⟨∑|qᵢ|𝐫ᵢ⟩/N| = {:.1} eÅ", coc.norm());
+        println!("charge center displacement    = |⟨∑|qᵢ|𝐫ᵢ⟩/N| = {:.1} eÅ", coc.norm());
+
         let mu = self.dipole_moment.transpose() / self.number_of_samples as f64;
-        println!("𝛍 = |⟨∑qᵢ𝐫ᵢ⟩|       = {:.1} eÅ", mu.norm());
+        println!("dipole moment 𝛍               = |⟨∑qᵢ𝐫ᵢ⟩| = {:.1} eÅ", mu.norm());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// Copyright (c) 2022 Mikael Lund
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 extern crate num_traits;
 
 mod energy;
@@ -7,6 +27,7 @@ mod input;
 mod output;
 mod analysis;
 
+use indicatif::{ProgressBar, ProgressStyle};
 use clap::Parser;
 use std::error::Error;
 use analysis::Moments;
@@ -29,11 +50,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     propagator.push(SwapCharges::default());
 
     // main Monte Carlo loop
+    let bar = ProgressBar::new(args.steps as u64);
+    bar.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})")
+        .progress_chars("#>-"));
     for _ in 0..args.steps {
+        bar.inc(1);
         propagator.do_move(&hamiltonian, &mut particles, &mut rng);
         moments.sample(&particles);
     }
-    println!("number of samples = {}", args.steps);
+    bar.finish();
     propagator.print();
     moments.print();
 
